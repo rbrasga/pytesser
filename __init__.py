041685202fa3137cd82bce1e52ede1d6b314e572
Remington Brasga <Remington.Brasga@taec.toshiba.com>
2018-10-10 20:06:57 -0700

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,22 @@
+"""Pillow {} (Fork of the Python Imaging Library)
+
+Pillow is the friendly PIL fork by Alex Clark and Contributors.
+    https://github.com/python-pillow/Pillow/
+
+Pillow is forked from PIL 1.1.7.
+
+PIL is the Python Imaging Library by Fredrik Lundh and Contributors.
+Copyright (c) 1999 by Secret Labs AB.
+
+Use PIL.__version__ for this Pillow version.
+PIL.VERSION is the old PIL version and will be removed in the future.
+
+;-)
+"""
+
+# VERSION is deprecated and will be removed in Pillow 6.0.0.
+# PILLOW_VERSION is deprecated and will be removed after that.
+# Use __version__ instead.
+VERSION = '0.0.1'  # PIL Version
+
+_plugins = []
